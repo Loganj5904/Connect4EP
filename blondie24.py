3c750737eb708ffe86fbc2b7ea1d33844a38d350
agustin-lorenzo <lorenzo.agustin.al@gmail.com>
2024-11-21 13:37:25 -0500

    implemented network, began blondie ES algorithm, haven't tested

--- blondie24.py
+++ blondie24.py
@@ -1,85 +1,89 @@
-import connect4
-import numpy as np
-import random
+import network
+import connect4 as c4
 import copy
-from math import exp
+import random
+import math
+import pickle
 
-tau = 0.0839
-
-
-def tanh(x):
-    return (exp(x)-exp(-x))/(exp(x)+exp(-x))
-
-
-def getSubsquares(board, rows, cols, startSize=3):
-    allSubsquares = []  
-
-    for height in range(startSize, rows + 1):  
-        for width in range(startSize, cols + 1): 
-            subsquareList = []  
-
-            for row in range(rows - height + 1):
-                for col in range(cols - width + 1):
-                    subsquare = []
-
-                    for h in range(height):  
-                        for w in range(width): 
-                            index = (row + h) * cols + (col + w)
-                            subsquare.append(board[index])
-
-                    subsquareList.append(subsquare)
-            allSubsquares.append(subsquareList)
-
-    def flattenOutput(subsquares):
-        return [item for sublist in subsquares for subsublist in sublist for item in subsublist]
-    
-    return flattenOutput(allSubsquares)
+def getPossibleMoves(board):
+    moves = []
+    for i in range(7):
+        boardCopy = copy.deepcopy(board)
+        if c4.place(boardCopy, 1, i):
+            moves.append(i)
+    return moves
 
 
-class Network():
-    def __init__(self, inputSize = 2600):
-        self.kingValue = 2
-        self.sigma = 0.05
+class Blondie(network.Network):
 
-        self.weightsInputHidden1 = np.random.randn(inputSize, 91) * 0.2 
-        self.biasHidden1 = np.random.randn((1, 91)) * 0.2
-
-        self.weightsHidden1Hidden2 = np.random.randn(91, 40) * 0.2
-        self.biasHidden2 = np.randn((1, 40)) * 0.2
-
-        self.weightsHidden2Output = np.random.randn(91, 10) * 0.2
-        self.biasOutput = np.randn((1, 10)) * 0.2
-    
-    def forward(self, X):
-        self.hidden1Input = np.dot(X, self.weightsInputHidden1) + self.biasHidden1
-        self.hidden1Output = tanh(self.hidden1Input)
-
-        self.hidden2Input = np.dot(self.hidden1Input, self.weightsHidden1Hidden2) + self.biasHidden2
-        self.hidden2Output = tanh(self.hidden2Input)
-
-        self.outputInput = np.dot(self.hidden2Output, self.weightsHidden2Output) + self.biasOutput
-        self.outputInput += sum(X[-42:]) # sum of original board inputs
-        return tanh(self.outputInput)
-
-
-    def evaluate(self, board):
-        subsquares = getSubsquares(connect4.getBoardString(board), 6, 7)
-        return self.forward(subsquares)
-    
-
-    def createOffspring(self):
-        offspring = copy.deepcopy(self)
-        offspring.sigma = offspring.sigma * exp(tau*random.gauss(0,1))
-
-        def mutate(l):
-            l = [wj + offspring.sigma * random.gauss(0,1) for wj in l]
+    def minimax(self, board, team=1, depth=8):
+        if depth == 0:
+            return self.evaluate(board), None
         
-        mutate(offspring.weightsInputHidden1)
-        mutate(offspring.biasHidden1)
-        mutate(offspring.weightsHidden1Hidden2)
-        mutate(offspring.biasHidden2)
-        mutate(offspring.weightsHidden2Output)
-        mutate(offspring.biasOutput)
+        if team == 1:
+            bestScore = -math.inf
+        else:
+            bestScore = math.inf
 
-        return offspring
+        bestMove = None
+        for move in getPossibleMoves(board):
+            boardCopy = copy.deepcopy(board)
+            if not c4.place(boardCopy, team, move):
+                continue
+            score, _ = self.minimax(boardCopy, -team, depth-1)
+            
+            if team ==1:
+                if score > bestScore:
+                    bestScore = score
+                    bestMove = move
+            else:
+                if score < bestScore:
+                    bestScore = score
+                    bestMove = move
+        
+        return bestScore, bestMove
+
+def playGame(player1, player2):
+    board = c4.createBoard()
+    
+    while not c4.boardFull(board):
+        _, player1Move = player1.minimax(board)
+        c4.place(board, 1, player1Move)
+        if c4.checkWin(board) == 1: return 1
+
+        _, player2Move = player2.minimax(board)
+        c4.place(board, -1, player2Move)
+        if c4.checkWin(board) == -1: return -1
+
+    return 0
+
+
+def runES(generations = 840):
+    networks = [Blondie() for _ in range(15)]
+    offspring = [network.createOffspring() for network in networks]
+    networks.append(offspring)
+
+    for _ in range(generations):
+
+        for currentNetwork in networks:
+            possibleOpponents = [n for n in networks if n is not currentNetwork]
+            selctedOpponents = random.choices(possibleOpponents, k=5)
+
+            for opponent in selctedOpponents:
+                if playGame(currentNetwork, opponent) == 1: currentNetwork.fitness += 1
+                if playGame(currentNetwork, opponent) == 0: currentNetwork.fitness += 0
+                if playGame(currentNetwork, opponent) == -1: currentNetwork.fitness += -2
+            
+        networks.sort(key=lambda g: g.fitness)
+        networks = networks[:15] # select survivors
+        offspring = [network.createOffspring() for network in networks]
+        networks.append(offspring)
+    
+    networks.sort(key=lambda g: g.fitness)
+    return networks[-1]
+
+bestNetwork = runES()
+with open('bestNetwork.pkl', 'wb') as f:
+    pickle.dump(bestNetwork, f)
+
 
